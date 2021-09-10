fb3cec41ce6e6a6bba2dd73397941927f882dc6f
vinipolitta <vinicius.politta1@gmail.com>
2021-09-10 13:18:53 -0300

    Seção 6: Angular - Organizando Projeto (V1)

--- ProAgil/ProAgil-WEB/src/app/app.module.ts
+++ ProAgil/ProAgil-WEB/src/app/app.module.ts
@@ -1,26 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { EventoService } from './services/evento.service';
+
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
-import { HttpClientModule } from '@angular/common/http';
 import { NavBarComponent } from './navBar/navBar.component';
-import { FormsModule } from '@angular/forms';
+
+import { DateTimeFormatPipePipe } from './helper/DateTimeFormatPipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
       EventosComponent,
-      NavBarComponent
+      NavBarComponent,
+      DateTimeFormatPipePipe
    ],
   imports: [
     BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     FormsModule
   ],
-  providers: [],
+  providers: [EventoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ProAgil/ProAgil-WEB/src/app/eventos/eventos.component.ts
+++ ProAgil/ProAgil-WEB/src/app/eventos/eventos.component.ts
@@ -1,13 +1,26 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Evento } from '../models/Evento';
+import { EventoService } from '../services/evento.service';
 
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.css']
+  styleUrls: ['./eventos.component.css'],
 })
 export class EventosComponent implements OnInit {
-   _filtroLista: any;
+   _filtroLista: any;   
+  eventosFiltrados: Evento[] = [];
+  eventos: Evento[] = [];
+  imagemLargura = 50;
+  imagemMargem = 2;
+  mostrarImagem = false;
+  modalRef!: BsModalRef;
+  
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService
+  ) { }
 
    get filtroLista(): string {
     return this._filtroLista;
@@ -18,40 +31,39 @@ export class EventosComponent implements OnInit {
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos
   }
 
-  eventosFiltrados: any = [];
-
-  eventos: any = [];
-  imagemLargura = 50;
-  imagemMargem = 2;
-  mostrarImagem = false;
-  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
   }
 
+  // ALTERNA A IMAGEM PELO BTN
   alternarImagem() {
     this.mostrarImagem = !this.mostrarImagem;
   }
 
-  filtrarEventos(filtrarPor: any): any {
+  // FILTRA OS EVENTOS PELO INPUT BUSCA
+  filtrarEventos(filtrarPor: string): Evento[] {
     filtrarPor = filtrarPor.toLocaleLowerCase();
     return this.eventos.filter(
-      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
     );
-
   }
 
-  getEventos() {
-    this.http.get('http://localhost:5000/api/values').subscribe(res => {
-      this.eventos = res
-      console.log(res);
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template)
+  }
 
-    },
-    error => {
-      console.log(error);
-    }
+  // LISTA DOS EVENTOS VINDO DO BACKEND
+  getEventos() {
+    this.eventoService.getAllEventos().subscribe((_eventos: Evento[]) => 
+      {
+        this.eventos = _eventos
+        this.eventosFiltrados = this.eventos;
+        console.log(_eventos);
+      },
+      error => {
+        console.log(error);
+      }
     )
   }
-
 }
