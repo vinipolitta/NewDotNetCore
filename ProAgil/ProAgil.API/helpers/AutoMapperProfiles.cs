09643d39002f56d37cbe3f1e359a2b4a7b074e7c
Vinicius <vinicius.politta1@gmail.com>
2021-09-13 10:46:52 -0300

    Seção 11: Asp.NET Core Identity - (TOKEN - JWT) (V1)

--- ProAgil/ProAgil.API/helpers/AutoMapperProfiles.cs
+++ ProAgil/ProAgil.API/helpers/AutoMapperProfiles.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using AutoMapper;
 using ProAgil.API.Dtos;
 using ProAgil.Domain;
+using ProAgil.Domain.Identity;
 using ProAgil.Domain.Models;
 
 namespace ProAgil.API.helpers
@@ -22,6 +23,11 @@ namespace ProAgil.API.helpers
 
             CreateMap<Lote, LoteDto>().ReverseMap();
             CreateMap<RedeSocial, RedeSocialDto>().ReverseMap();
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<User, UserLoginDto>().ReverseMap();
+
+
+
 
         }
     }
