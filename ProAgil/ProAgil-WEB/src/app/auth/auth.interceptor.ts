d296efad067d8f7071cd4e7fc1e212ee5b9e1272
Vinicius <vinicius.politta1@gmail.com>
2021-09-24 12:59:14 -0300

    publish dotnet

--- ProAgil/ProAgil-WEB/src/app/auth/auth.interceptor.ts
+++ ProAgil/ProAgil-WEB/src/app/auth/auth.interceptor.ts
@@ -2,7 +2,7 @@ import {  HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { Observable } from "rxjs";
-import { tap } from 'rxjs/internal/operators/tap'
+import { tap } from "rxjs/operators";
 
 
 @Injectable({ providedIn: 'root' })
